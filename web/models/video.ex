873182e21f548c75f03f96f4d210d1b2c1b4711e
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2016-09-11 06:46:06 -0700

    Add and seed categories

--- web/models/video.ex
+++ web/models/video.ex
@@ -6,6 +6,7 @@ defmodule Rumbl.Video do
     field :title, :string
     field :description, :string
     belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
 
     timestamps
   end
@@ -15,7 +16,7 @@ defmodule Rumbl.Video do
   """
 
   @required_fields ~w(url title description)
-  @optional_fields ~w()
+  @optional_fields ~w(category_id)
 
   def changeset(struct, params \\ %{}) do
     struct

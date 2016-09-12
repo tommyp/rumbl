a4aead611b0244b40c72314ffaefad46188eb24b
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2016-09-12 06:40:31 +0100

    Associate videos with categories

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -13,6 +13,6 @@
 alias Rumbl.Repo
 alias Rumbl.Category
 
-for category <- ~w(Action Drama Romance Comedy Sci-f) do
+for category <- ~w(Action Drama Romance Comedy Sci-fi) do
   Repo.get_by(Category, name: category) || Repo.insert!(%Category{name: category})
 end

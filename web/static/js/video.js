66c0973ca9b407ca36f473a46858e77e7ebdba81
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2016-10-24 23:52:42 +0100

    Handle disconnections

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -25,6 +25,7 @@ let Video = {
     })
 
     vidChannel.on("new_annotation", (resp) => {
+      vidChannel.params.last_seen_id = resp.id
       this.renderAnnotation(msgContainer, resp)
     })
 
@@ -37,6 +38,10 @@ let Video = {
 
     vidChannel.join()
       .receive("ok", resp => {
+        let ids = resp.annotations.map(ann => ann.id)
+        if (ids.length > 0) {
+          vidChannel.params.last_seen_id = Math.max(...ids)
+        }
         this.scheduleMessages(msgContainer, resp.annotations)
       })
       .receive("error", reason => console.log("join failed", reason) )

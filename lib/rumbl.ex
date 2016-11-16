33f31135564d33ca6ddc80f8df168aca3cc56624
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2016-11-16 23:46:15 +0000

    Remove Counter app from supervision tree

--- lib/rumbl.ex
+++ lib/rumbl.ex
@@ -12,8 +12,6 @@ defmodule Rumbl do
       supervisor(Rumbl.Repo, []),
       # Start the endpoint when the application starts
       supervisor(Rumbl.Endpoint, []),
-
-      worker(Rumbl.Counter, [5])
       # Start your own worker by calling: Rumbl.Worker.start_link(arg1, arg2, arg3)
       # worker(Rumbl.Worker, [arg1, arg2, arg3]),
     ]
